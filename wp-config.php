65eba145452c0d9b92c97921c0e5dc6982daab64
Myung Ko <mko@weareratio.com>
2015-01-05 11:44:22 -0800

    DBName change

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'WP');
+define('DB_NAME', 'myungwptesting');
 
 /** MySQL database username */
 define('DB_USER', 'root');
